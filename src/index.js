e151ff0b8d5823ae2e1257f357b080997804f1ae
Felipe Oliveira <felpd3v@gmail.com>
2022-11-17 18:25:37 -0300

    feat: create and export a window

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export const Window = ({ title, children }) => {
+  return (
+    <div className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">{title}</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize"></button>
+          <button aria-label="Maximize"></button>
+          <button aria-label="Close"></button>
+        </div>
+      </div>
+      <div className="window-body">{children}</div>
+    </div>
+  );
+};
