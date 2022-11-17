8695c2433230ef5de910c8b9b7d2218f7960a57f
Felipe Oliveira <felpd3v@gmail.com>
2022-11-17 18:27:54 -0300

    chore: import 98.css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import "98.css/dist/98.css"
+
 export const Window = ({ title, children }) => {
   return (
-    <div className="window">
-      <div className="title-bar">
-        <div className="title-bar-text">{title}</div>
-        <div className="title-bar-controls">
-          <button aria-label="Minimize"></button>
-          <button aria-label="Maximize"></button>
-          <button aria-label="Close"></button>
+    <div className='window'>
+      <div className='title-bar'>
+        <div className='title-bar-text'>{title}</div>
+        <div className='title-bar-controls'>
+          <button aria-label='Minimize'></button>
+          <button aria-label='Maximize'></button>
+          <button aria-label='Close'></button>
         </div>
       </div>
-      <div className="window-body">{children}</div>
+      <div className='window-body'>{children}</div>
     </div>
-  );
-};
+  )
+}
